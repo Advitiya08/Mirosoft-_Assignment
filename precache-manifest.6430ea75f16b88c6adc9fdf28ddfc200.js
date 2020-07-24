a4aa7e0e0d359c32be084d597831c57f5ab52224
Advitiya08 <sri.advitiya@gmail.com>
2020-07-24 15:22:15 +0530

    Updates

---
+++ precache-manifest.6430ea75f16b88c6adc9fdf28ddfc200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d0ee20d55658780973b65987280af4",
+    "url": "/Mirosoft-_Assignment/index.html"
+  },
+  {
+    "revision": "fdcc74a713199ffe906a",
+    "url": "/Mirosoft-_Assignment/static/css/2.7f6cef89.chunk.css"
+  },
+  {
+    "revision": "1f01189e36ffbaa62caf",
+    "url": "/Mirosoft-_Assignment/static/css/main.2891cd6e.chunk.css"
+  },
+  {
+    "revision": "fdcc74a713199ffe906a",
+    "url": "/Mirosoft-_Assignment/static/js/2.adfa281d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mirosoft-_Assignment/static/js/2.adfa281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f01189e36ffbaa62caf",
+    "url": "/Mirosoft-_Assignment/static/js/main.10be4c13.chunk.js"
+  },
+  {
+    "revision": "166a47b0919a90f49e2d",
+    "url": "/Mirosoft-_Assignment/static/js/runtime-main.18fa1201.js"
+  }
+]);
